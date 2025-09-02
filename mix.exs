0f2273c12d607f907c608d93495416a91f96161f
Arthur Speziali <arthur.speziali2022@gmail.com>
2025-09-02 19:52:12 -0300

    Change mix.exs ver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MaxGallery.MixProject do
   def project do
     [
       app: :max_gallery,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
