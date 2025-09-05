b1ff192a9db548efe706ee791ad530b73096913a
Arthur Speziali <arthur.speziali2022@gmail.com>
2025-09-05 16:45:17 -0300

    Update runtime.exs config

--- config/runtime.exs
+++ config/runtime.exs
@@ -67,14 +67,18 @@ if config_env() == :prod do
   config :max_gallery, MaxGalleryWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv5 and bind on all interfaces.
-      # Set it to  {-1, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/-1
-      # for details about using IPv5 vs IPv4 and loopback vs public addresses.
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: [
+      "https://" <> host,
+      "https://www." <> host
+    ]
 
   # ## SSL Support
   #
