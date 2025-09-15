fcd93c155405ec9b0ce4b86a3aea8c8e1e4c0188
Arthur Speziali <arthur.speziali2022@gmail.com>
2025-09-15 15:56:08 -0300

    Migration

--- priv/repo/migrations/20250914170025_allow_null_blob_iv.exs
+++
@@ -1,9 +0,0 @@
-defmodule MaxGallery.Repo.Migrations.AllowNullBlobIv do
-  use Ecto.Migration
-
-  def change do
-    alter table("cyphers") do
-      modify :blob_iv, :binary, null: true
-    end
-  end
-end
